d99e167a499e566087935b43a3c0b1450ca9b8e3
HaramSs <haram8274@gmail.com>
2024-08-08 17:14:40 +0900

    t

--- src/spark_flow/api.py
+++ src/spark_flow/api.py
@@ -3,15 +3,25 @@ import os
 import shutil
 
 def re_partition(load_dt, from_path='/data/movie/extract'):
-    home_dir = os.path.expanduser("~")
-    read_path = f'{home_dir}/{from_path}/load_dt={load_dt}'
+    home_dir = os.path.expanduser("~") # /Home/HaramSs
+
+    # /Home/HaramSs/data/movie/extract
+    read_path = os.path.join(home_dir, 'data','movie','extract')
     write_path = f'{home_dir}/data/movie/repartition'
+
     df = pd.read_parquet(read_path)
+
+    # DataFrame에 "load_dt" 컬럼을 생성 load_dt컬럼에는 load_dt값이 들어간다.
     df['load_dt'] = load_dt
+
+    # df의 DataFrame을 wirte_path에 parquet파일로 생성 
+    # 그리고 파티셔닝은 load_dt, multiMovieYn, repNationCd 기준으로
     df.to_parquet(
             write_path,
             partition_cols=['load_dt', 'multiMovieYn', 'repNationCd']
             )
+    
+    # 결과 반환은 df의 길이 (row 수), 파일을 읽는 경로, 파일을 쓰는 경로
     return len(df), read_path, f'{write_path}/load_dt={load_dt}'
 
 def rm_dir(dir_path):
