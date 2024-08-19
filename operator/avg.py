64ea47e4f931298774f5041089b6a52fd2bb53b0
HaramSs <haram8274@gmail.com>
2024-08-19 09:46:55 +0900

    add

--- operator/avg.py
+++ operator/avg.py
@@ -4,4 +4,13 @@ import sys
 app_name = sys.argv[1]
 load_dt = sys.argv[2]
 
-spark = SparkSession.builder.appName(app_name).getOrCreate()
+spark = SparkSession.builder.appName(Simple_sum).getOrCreate()
+
+df1 = spark.read.parquet(f"/home/haram/data/movie/hive/load_dt={LOAD_DT}")
+
+df1.createOrReplaceTempView("one_day")
+
+df_n = spark.sql(f"""
+SELECT
+    sum(salesAmt) as sum_salesAmt,
+    sum(audiCnt) as sum_audiCnt
